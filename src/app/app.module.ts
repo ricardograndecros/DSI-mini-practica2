4555f9c9023ffeece35caba6afb48df0b016b5e8
carmenabella <100408887@alumnos.uc3m.es>
2022-03-11 12:37:39 +0100

    column sort

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { MainTableComponent } from './main-table/main-table.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { SearchfilterPipe } from './searchfilter.pipe';
+import { FormsModule} from '@angular/forms'
 
 @NgModule({
   declarations: [
--- src/app/main-table/main-table.component.ts
+++ src/app/main-table/main-table.component.ts
@@ -19,6 +19,7 @@ export class MainTableComponent implements OnInit {
   searchMode: string = "Simple";
   maxprice: number = 230000;
   prueba=""
+  table_headers: string[] = ["Photo","Brand","Model","Year","Available_since","Price","PVP"];
 
   constructor() { }
 
@@ -34,6 +35,15 @@ export class MainTableComponent implements OnInit {
   }
   deleteProduct(index: number){
     this.products.splice(index, 1);
+  }
+
+  
+  sort(colName: string) {
+    if (colName == 'brand') this.products.sort((a: Product, b: Product) => a.brand > b.brand ? 1 : a.brand < b.brand ? -1 : 0)
+    if (colName == 'model') this.products.sort((a: Product, b: Product) => a.model > b.model ? 1 : a.model < b.model ? -1 : 0)
+    if (colName == 'year') this.products.sort((a: Product, b: Product) => a.year > b.year ? 1 : a.year < b.year ? -1 : 0)
+    if (colName == 'available_since') this.products.sort((a: Product, b: Product) => a.available_since > b.available_since ? 1 : a.available_since < b.available_since ? -1 : 0)
+    if (colName == 'price') this.products.sort((a: Product, b: Product) => a.price > b.price ? 1 : a.price < b.price ? -1 : 0)
   }
 
   setSearch(){
